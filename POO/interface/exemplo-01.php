a23048860c88676da87b19548c87b2bd847bf4ee
oo7gabriel <gabrielfsacademico@gmail.com>
2020-08-15 15:18:04 -0300

    Finalizando a Seção 9 :white_check_mark:

---
+++ POO/interface/exemplo-01.php
@@ -0,0 +1,30 @@
+<?php
+
+interface Veiculo {
+
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+}
+
+class Civic implements Veiculo {
+
+    public function acelerar($velocidade){
+        echo "O veiculo acelorou até " . $velocidade . " km/h"; 
+    }
+
+    public function frenar($velocidade){
+        echo "O veiculo frenou até " . $velocidade . " km/h";   
+    }
+
+    public function trocarMarcha($marcha){
+        echo "O veículo engatou a marcha " . $marcha;
+    }
+
+}
+
+$carro = new Civic();
+
+$carro->trocarMarcha(1);
+
+?>
